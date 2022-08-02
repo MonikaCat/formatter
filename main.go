1efc6416a877b1bae09d78f55d6a34671fa0cbd2
Magic Cat <pusz.monika@gmail.com>
2022-08-02 20:31:08 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	yaml "gopkg.in/yaml.v3"
+)
+
+func main() {
+	file := flag.String("filename", "", "filename")
+	fileShort := flag.String("f", "", "filename")
+	flag.Parse()
+
+	if *file == "" && *fileShort != "" {
+		file = fileShort
+	}
+	var (
+		yamlFile      *os.File
+		yamlFileOut   *os.File
+		err           error
+		overwriteFile bool
+	)
+	if *file != "" {
+		overwriteFile = true
+		yamlFile, err = os.OpenFile(*file, os.O_RDWR, 0644)
+		if err != nil {
+			log.Fatalf("could not open file: %v", err)
+		}
+
+		yamlFileOut = yamlFile
+		defer yamlFile.Close()
+	} else {
+		yamlFile = os.Stdin
+		yamlFileOut = os.Stdout
+	}
+
+	var out interface{}
+	if err = yaml.NewDecoder(yamlFile).Decode(&out); err != nil {
+		log.Fatalf("could not decode file: %v", err)
+	}
+
+	if overwriteFile {
+		if err = yamlFileOut.Truncate(0); err != nil {
+			log.Fatalf("could not truncate file: %v", err)
+		}
+		if _, err = yamlFileOut.Seek(0, 0); err != nil {
+			log.Fatalf("could not seek file: %v", err)
+		}
+	}
+
+	err = yaml.NewEncoder(yamlFileOut).Encode(out)
+	if err != nil {
+		log.Fatalf("could not encode file: %v", err)
+	}
+}
